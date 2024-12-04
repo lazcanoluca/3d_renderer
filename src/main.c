d2ededf87979349abfac3772e20df50aa5411296
lazcanoluca <lazcanoluca@gmail.com>
2024-12-04 18:47:42 -0300

    alloc color buffer & cleanup

--- src/main.c
+++ src/main.c
@@ -1,14 +1,19 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_events.h>
-#include <SDL2/SDL_keycode.h>
-#include <SDL2/SDL_mouse.h>
-#include <SDL2/SDL_render.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define W_WIDTH 800
+#define W_HEIGHT 600
 
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 bool is_running = false;
+
+typedef uint32_t rgba_t;
+
+rgba_t *color_buffer = NULL;
 
 bool init_window(void) {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -17,10 +22,9 @@ bool init_window(void) {
     return false;
   }
 
-  // Create an SDL Window.
   window =
       SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                       800, 600, SDL_WINDOW_BORDERLESS);
+                       W_WIDTH, W_HEIGHT, SDL_WINDOW_BORDERLESS);
 
   if (!window) {
     fprintf(stderr, "Error creating SDL window.\n");
@@ -54,7 +58,16 @@ void process_input() {
   }
 }
 
-void setup(void) {}
+void setup(void) {
+  color_buffer = (rgba_t *)malloc(sizeof(rgba_t) * W_WIDTH + W_HEIGHT);
+}
+
+void destroy_window(void) {
+  free(color_buffer);
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+}
 
 void update(void) {}
 
@@ -75,6 +88,8 @@ int main(void) {
     update();
     render();
   }
+
+  destroy_window();
 
   return 0;
 }
