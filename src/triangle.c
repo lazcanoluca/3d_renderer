3d9cfe7575e2e9e3cfde2744ab7b9c83cbb9efbb
lazcanoluca <lazcanoluca@gmail.com>
2025-03-16 18:16:32 -0300

    fix: consider triangles without top or bottom half to avoid div by 0 when filling

--- src/triangle.c
+++ src/triangle.c
@@ -64,9 +64,17 @@ void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2,
     int_swap(&x0, &x1);
   }
 
-  int my = y1;
-  int mx = (float)((x2 - x0) * (y1 - y0)) / (float)(y2 - y0) + x0;
+  if (y1 == y2) {
+    // there is no bottom half
+    fill_flat_bottom_triangle(x0, y0, x1, y1, x2, y2, color);
+  } else if (y0 == y1) {
+    // there is no top half
+    fill_flat_top_triangle(x0, y0, x1, y1, x2, y2, color);
+  } else {
+    int my = y1;
+    int mx = (float)((x2 - x0) * (y1 - y0)) / (float)(y2 - y0) + x0;
 
-  fill_flat_bottom_triangle(x0, y0, x1, y1, mx, my, color);
-  fill_flat_top_triangle(x1, y1, mx, my, x2, y2, color);
+    fill_flat_bottom_triangle(x0, y0, x1, y1, mx, my, color);
+    fill_flat_top_triangle(x1, y1, mx, my, x2, y2, color);
+  }
 }
