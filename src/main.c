edbf79c6e74982c119b9187e30b8ef45646363ac
lazcanoluca <lazcanoluca@gmail.com>
2025-03-31 15:50:21 -0300

    chore: world matrix

--- src/main.c
+++ src/main.c
@@ -125,6 +125,8 @@ void update(void) {
   mesh.rotation.z += 0.01;
 
   mesh.scale.x += 0.002;
+  mesh.scale.y += 0.001;
+
   mesh.translation.x += 0.01;
   mesh.translation.z = 5;
 
@@ -150,21 +152,16 @@ void update(void) {
     for (int j = 0; j < 3; j++) {
       vec4_t transformed_vertex = vec4_from_vec3(face_vertices[j]);
 
-      /*transformed_vertex = vec3_rotate_x(transformed_vertex,
-       * mesh.rotation.x);*/
-      /*transformed_vertex = vec3_rotate_y(transformed_vertex,
-       * mesh.rotation.y);*/
-      /*transformed_vertex = vec3_rotate_z(transformed_vertex,
-       * mesh.rotation.z);*/
-      /**/
+      // Create world matrix combining scale, rotation and translation.
+      mat4_t world_matrix = mat4_identity();
+      world_matrix = mat4_mul_mat4(scale_matrix, world_matrix);
+      world_matrix = mat4_mul_mat4(rotation_matrix_x, world_matrix);
+      world_matrix = mat4_mul_mat4(rotation_matrix_y, world_matrix);
+      world_matrix = mat4_mul_mat4(rotation_matrix_z, world_matrix);
+      world_matrix = mat4_mul_mat4(translation_matrix, world_matrix);
 
-      transformed_vertex = mat4_mul_vec(scale_matrix, transformed_vertex);
-      transformed_vertex = mat4_mul_vec(rotation_matrix_x, transformed_vertex);
-      transformed_vertex = mat4_mul_vec(rotation_matrix_y, transformed_vertex);
-      transformed_vertex = mat4_mul_vec(rotation_matrix_z, transformed_vertex);
-      transformed_vertex = mat4_mul_vec(translation_matrix, transformed_vertex);
-
-      /*transformed_vertex.z += 5;*/
+      // Transform vector by world matrix.
+      transformed_vertex = mat4_mul_vec4(world_matrix, transformed_vertex);
 
       transformed_vertices[j] = transformed_vertex;
     }
--- src/matrix.h
+++ src/matrix.h
@@ -12,6 +12,7 @@ mat4_t mat4_translation(float tx, float ty, float tz);
 mat4_t mat4_rotation_x(float angle);
 mat4_t mat4_rotation_y(float angle);
 mat4_t mat4_rotation_z(float angle);
-vec4_t mat4_mul_vec(mat4_t m, vec4_t v);
+vec4_t mat4_mul_vec4(mat4_t m, vec4_t v);
+mat4_t mat4_mul_mat4(mat4_t a, mat4_t b);
 
 #endif
