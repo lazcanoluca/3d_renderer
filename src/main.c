db1ebe8cb5e60cb0730552379314854f5b82b2c6
Luca Lazcano <lazcanoluca@gmail.com>
2025-12-29 18:19:14 -0300

    fix: triangle sorting loop to out of triangle faces in update which caused it to run super slow

--- src/main.c
+++ src/main.c
@@ -281,22 +281,22 @@ void update(void) {
             .color = triangle_color,
             .avg_depth = avg_depth};
 
-        // Sorting the triangles the render by their average depth
-        int num_triangles = array_length(triangles_to_render);
-        for (int i = 0; i < num_triangles; i++) {
-            for (int j = i; j < num_triangles; j++) {
-                if (triangles_to_render[i].avg_depth <
-                    triangles_to_render[j].avg_depth) {
-                    triangle_t temp = triangles_to_render[i];
-                    triangles_to_render[i] = triangles_to_render[j];
-                    triangles_to_render[j] = temp;
-                }
-            }
-        }
-
         // save the projected triangle in the array of triangles to render
         /*triangles_to_render[i] = projected_triangle;*/
         array_push(triangles_to_render, projected_triangle);
+    }
+
+    // Sorting the triangles the render by their average depth
+    int num_triangles = array_length(triangles_to_render);
+    for (int i = 0; i < num_triangles; i++) {
+        for (int j = i; j < num_triangles; j++) {
+            if (triangles_to_render[i].avg_depth <
+                triangles_to_render[j].avg_depth) {
+                triangle_t temp = triangles_to_render[i];
+                triangles_to_render[i] = triangles_to_render[j];
+                triangles_to_render[j] = temp;
+            }
+        }
     }
 }
 
